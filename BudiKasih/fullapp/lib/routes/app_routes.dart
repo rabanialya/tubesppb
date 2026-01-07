62b6905ea599b9e2875549b485d0a45a9217188f
rengganis <rengganistantri4@gmail.com>
2026-01-08 00:15:06 +0700

    perbaikan route biar ga error

--- BudiKasih/fullapp/lib/routes/app_routes.dart
+++ BudiKasih/fullapp/lib/routes/app_routes.dart
@@ -11,16 +11,18 @@ import '../pages/auth/setting_pass_page.dart';
 import '../pages/dashboard/profile/donation_history_page.dart';
 
 class AppRoutes {
-  static Map<String, WidgetBuilder> routes = {
-    '/': (context) => const WelcomePage(),
-    '/login': (context) => const LoginPage(),
-    '/signup': (context) => const SignUpPage(),
-    '/home': (context) => const HomePage(),
-    '/donationgoods': (context) => const DonationGoodsPage(),
-    '/donationcash': (context) => const DonationCashPage(),
-    '/profile': (context) => const ProfilePage(),
-    '/notification': (context) => const NotificationPage(),
-    '/setting_pass': (context) => const SettingPassPage(),
-    '/history': (context) => const DonationHistoryPage(),
-  };
+  static Map<String, WidgetBuilder> get routes {
+    return {
+      '/': (context) => const WelcomePage(),
+      '/login': (context) => const LoginPage(),
+      '/signup': (context) => const SignUpPage(),
+      '/home': (context) => const HomePage(),
+      '/donationgoods': (context) => const DonationGoodsPage(),
+      '/donationcash': (context) => const DonationCashPage(),
+      '/profile': (context) => const ProfilePage(),
+      '/notification': (context) => const NotificationPage(),
+      '/setting_pass': (context) => const SettingPassPage(),
+      '/history': (context) => const DonationHistoryPage(),
+    };
+  }
 }
